99ec59d49f404a05497625b0fbdaaa680c0e6c7d
Kshitiz Srivastava <ksh1998@gmail.com>
2020-07-08 13:39:35 +0530

    improvement

--- app.py
+++ app.py
@@ -9,14 +9,36 @@ def generate_problem_link(contestId, index):
 		return f"https://codeforces.com/contest/{contestId}/problem/{index}"
 	return f"https://codeforces.com/problemset/gymProblem/{contestId}/{index}"
 
+def get_title(rating):
+	title = ""
+	if rating < 1200:
+		title = "Newbie"
+	elif rating < 1400:
+		title = "Pupil"
+	elif rating < 1600:
+		title = "Specialist"
+	elif rating < 1900:
+		title = "Expert"
+	elif rating < 2100:
+		title = "Candidate Master"
+	elif rating < 2300:
+		title = "Master"
+	elif rating < 2400:
+		title = "International Master"
+	elif rating < 2600:
+		title = "Grandmaster"
+	elif rating < 3000:
+		title = "International Grandmaster"
+	else:
+		title = "Legendary Grandmaster"
+	return title
+
 @app.route("/", methods=['POST', 'GET']) 
 def home_view(): 
 	if request.method == 'POST':
 		handle = request.form['user_handle']
 		print("User handle is:" + str(handle))
-		payload = {'handle': handle}
-		# flash("Connecting to Codeforces API...")
-		r = requests.get('https://codeforces.com/api/user.status', params=payload)
+		r = requests.get('https://codeforces.com/api/user.status', params={'handle': handle})
 		flash("Connected to Codeforces server.")
 		response_data = r.json()
 		status = response_data['status']
@@ -53,7 +75,22 @@ def home_view():
 			for index in unsolved_problem_by_index:
 				if len(unsolved_problem_by_index[index]) > 0:
 					final_dict.update({index: unsolved_problem_by_index[index]})
-			return render_template('home.html', status=status, handle=handle, total_unsolved=total_unsolved, final_dict=final_dict)
+			# Fetching User Info
+			r = requests.get('https://codeforces.com/api/user.info', params={'handles': handle})
+			response_data = r.json()
+			# print(response_data['result'][0])
+			user_info_full = response_data['result'][0]
+			user_info = {
+				'handle': handle,
+				'first_name': user_info_full['firstName'] if 'firstName' in user_info_full else "",
+				'last_name': user_info_full['lastName'] if 'lastName' in user_info_full else "",
+				'rating': user_info_full['rating'] if 'rating' in user_info_full else 0,
+				'title': get_title(int(user_info_full['rating'])) if 'rating' in user_info_full else "Newbie",
+				'max_rating': user_info_full['maxRating'] if 'maxRating' in user_info_full else 0,
+				'max_title': get_title(int(user_info_full['maxRating'])) if 'maxRating' in user_info_full else "Newbie",
+				'organization': user_info_full['organization'] if 'organization' in user_info_full else "",
+			}
+			return render_template('home.html', status=status, user_info=user_info, total_unsolved=total_unsolved, final_dict=final_dict)
 		else:
 			comment = response_data['comment']
 			return render_template('home.html', status=status, comment=comment)
