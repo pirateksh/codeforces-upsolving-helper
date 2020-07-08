24aecb152b7c9bd9d9739f8994f96511843bc558
Kshitiz Srivastava <ksh1998@gmail.com>
2020-07-08 11:16:31 +0530

    refactoring

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import requests, json
   
 app = Flask(__name__) 
-  
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+def generate_problem_link(contestId, index):
+	if len(contestId) <= 4:
+		return f"https://codeforces.com/contest/{contestId}/problem/{index}"
+	return f"https://codeforces.com/problemset/gymProblem/{contestId}/{index}"
+
 @app.route("/", methods=['POST', 'GET']) 
 def home_view(): 
 	if request.method == 'POST':
 		handle = request.form['user_handle']
 		print("User handle is:" + str(handle))
 		payload = {'handle': handle}
-		print("Connecting to Codeforces API...")
+		# flash("Connecting to Codeforces API...")
 		r = requests.get('https://codeforces.com/api/user.status', params=payload)
-		print("Connected to Codeforces server.")
+		flash("Connected to Codeforces server.")
 		response_data = r.json()
 		status = response_data['status']
 		if status == 'OK':
-			print("Status is OK")
 			submissions = response_data['result']
 			counter = 1
 			unsolved_problem_set = set([])
@@ -23,6 +28,7 @@ def home_view():
 			for submission in submissions:
 				verdict = submission['verdict']
 				problem = submission['problem']
+				handle = submission['author']['members'][0]['handle']
 				if verdict == 'OK':
 					solved_problem_set.add(json.dumps(problem))
 				else:
@@ -41,30 +47,15 @@ def home_view():
 				index = str(problem['index'])
 				letter = index[0]
 				name = str(problem['name'])
-				if len(contestId) <= 4:
-					link = f"https://codeforces.com/contest/{contestId}/problem/{index}"
-				else:
-					link = f"https://codeforces.com/problemset/gymProblem/{contestId}/{index}"
-				# chunk = f": {index}. {link}\n"
-				temp_list = []
-				temp_list.append(index)
-				temp_list.append(name)
-				temp_list.append(link)
+				link = generate_problem_link(contestId, index)
 				unsolved_problem_by_index[letter].append([index, name, link])
 			final_dict = {}
 			for index in unsolved_problem_by_index:
 				if len(unsolved_problem_by_index[index]) > 0:
 					final_dict.update({index: unsolved_problem_by_index[index]})
-
-			print("Unsolved Problems have been identified. Check 'unsolved.txt' file.")
-			# print(final_dict)
-			print("Status:" + str(status))
 			return render_template('home.html', status=status, handle=handle, total_unsolved=total_unsolved, final_dict=final_dict)
 		else:
 			comment = response_data['comment']
-			print("Invalid Request")
-			print(comment)
 			return render_template('home.html', status=status, comment=comment)
 	else:
-		print("This is Get")
 		return render_template('home.html') 
