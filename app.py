c65507ce65344982cb193e5498a752a4f25e1cec
Kshitiz Srivastava <ksh1998@gmail.com>
2020-07-10 01:21:31 +0530

    parallel divs solved, problemset problems link solved

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, url_for
 import requests, json
   
 app = Flask(__name__) 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-def generate_problem_link(contestId, index):
+INF = 10000
+
+
+def generate_problem_link(problemset_name ,contestId, index):
+	"""
+		Function to generate problem link using contestID and index.
+	"""
+	if problemset_name:
+		return f"https://codeforces.com/problemsets/{problemset_name}/problem/99999/{index}"
+	if contestId == -1:
+		return "#"
 	if len(contestId) <= 4:
 		return f"https://codeforces.com/contest/{contestId}/problem/{index}"
 	return f"https://codeforces.com/problemset/gymProblem/{contestId}/{index}"
 
+
 def get_title(rating):
+	"""
+		Function that returns [user's title, title's color] based on user's rating.
+	"""
 	title = ""
 	if rating < 1200:
 		title = ["Newbie", "grey-text"]
@@ -33,11 +47,14 @@ def get_title(rating):
 		title = ["Legendary Grandmaster", "red-text"]
 	return title
 
+
 @app.route("/", methods=['POST', 'GET']) 
 def home_view(): 
+
 	if request.method == 'POST':
+
 		handle = request.form['user_handle']
-		print("User handle is:" + str(handle))
+
 		try:
 			r = requests.get('https://codeforces.com/api/user.status', params={'handle': handle}, timeout=10)
 		except requests.exceptions.ConnectTimeout:
@@ -46,13 +63,18 @@ def home_view():
 		except requests.exceptions.ReadTimeout:
 			flash("ReadTimeout: Connected to Codeforces server but it took too long to respond. Try Again!", 'error')
 			return render_template('home.html') 
+
 		response_data = r.json()
 		status = response_data['status']
+
 		if status == 'OK':
+
 			submissions = response_data['result']
 			counter = 1
 			unsolved_problem_set = set([])
 			solved_problem_set = set([])
+
+			# Segregating solved and unsolved problems
 			for submission in submissions:
 				verdict = submission['verdict']
 				problem = submission['problem']
@@ -61,31 +83,65 @@ def home_view():
 					solved_problem_set.add(json.dumps(problem))
 				else:
 					unsolved_problem_set.add(json.dumps(problem))
+
+			# Further segregating dummy unsolved problems
 			for problem in solved_problem_set:
 				if problem in unsolved_problem_set:
 					unsolved_problem_set.remove(problem)
+
 			unsolved_problem_list = []
 			for problem in unsolved_problem_set:
 				unsolved_problem_list += [json.loads(problem)]
-			total_unsolved = len(unsolved_problem_list)
-			unsolved_problem_by_index = {'A':[], 'B':[], 'C':[], 'D':[], 'E':[], 'F':[],'G':[], 'H':[], 'I':[], 'J':[], 'K':[], 'L':[], 'M':[], 'N':[], 'O':[], 'P':[], 'R':[], 'S':[], 'T':[], 'U':[], 'V':[], 'W':[], 'X':[], 'Y':[], 'Z':[]}
+
+			solved_problem_list = []
+			for problem in solved_problem_set:
+				solved_problem_list += [json.loads(problem)]
+
+			"""
+				Further Segregation of dummy unsolved problems:
+				Checking for same problem which appears in parallel Div. 1 and Div. 2
+				contest with different contestId.
+			"""
+			dummy_unsolved_problem_list = []
+			for unsolved_problem in unsolved_problem_list:
+				for solved_problem in solved_problem_list:
+					if solved_problem['name'] == unsolved_problem['name']:
+						solved_contest_id = solved_problem['contestId'] if 'contestId' in solved_problem else -1
+						unsolved_contest_id = unsolved_problem['contestId'] if 'contestId' in unsolved_problem else -1
+
+						if unsolved_contest_id != -1 and (unsolved_contest_id + 1 == solved_contest_id or unsolved_contest_id - 1 == solved_contest_id):
+							# This problem appeared in both Div. 1 and Div. 2 rounds and has been solved in one of them
+							dummy_unsolved_problem_list.append(unsolved_problem)
+
+			# Calculating total unsolved problems
+			total_unsolved = len(unsolved_problem_list) - len(dummy_unsolved_problem_list)
+
+			# Segregating unsolved problems by levels
+			unsolved_problem_by_index = {'A':[], 'B':[], 'C':[], 'D':[], 'E':[], 'F':[],'G':[], 'H':[], 'I':[], 'J':[], 'K':[], 'L':[], 'M':[], 'N':[], 'O':[], 'P':[], 'R':[], 'S':[], 'T':[], 'U':[], 'V':[], 'W':[], 'X':[], 'Y':[], 'Z':[], '#':[]}
 			for problem in unsolved_problem_list:
-				contestId = str(problem['contestId'])
-				index = str(problem['index'])
-				letter = index[0]
-				if not letter.isalpha():
-					letter = chr(int(index) + 64)
-				name = str(problem['name'])
-				INF = 10000
-				rating = problem['rating'] if 'rating' in problem else INF
-				link = generate_problem_link(contestId, index)
-				unsolved_problem_by_index[letter].append([index, name, link, rating])
+				if problem not in dummy_unsolved_problem_list:
+					# Checking if it is not a dummy unsolved problem.
+					contestId = str(problem['contestId']) if 'contestId' in problem else -1
+					problemset_name = problem['problemsetName'] if 'problemsetName' in problem else ""
+					index = str(problem['index'])
+					letter = index[0]
+					if not letter.isalpha():
+						letter = chr(int(index) + 64)
+					name = str(problem['name'])
+					rating = problem['rating'] if 'rating' in problem else INF
+					link = generate_problem_link(problemset_name, contestId, index)
+					if letter in unsolved_problem_by_index:
+						unsolved_problem_by_index[letter].append([index, name, link, rating])
+					else:
+						unsolved_problem_by_index['#'].append([index, name, link, rating])
+
+			# Creating final dictionary to pass to template
 			final_dict = {}
 			for index in unsolved_problem_by_index:
 				if len(unsolved_problem_by_index[index]) > 0:
-					# print(unsolved_problem_by_index[index])
 					unsolved_problem_by_index[index] = sorted(unsolved_problem_by_index[index], key = lambda rating: rating[3])
 					final_dict.update({index: unsolved_problem_by_index[index]})
+
 			# Fetching User Info
 			try:
 				r = requests.get('https://codeforces.com/api/user.info', params={'handles': handle}, timeout=10)
@@ -95,6 +151,7 @@ def home_view():
 			except requests.exceptions.ReadTimeout:
 				flash("ReadTimeout: Connected to Codeforces server but it took too long to respond. Try Again!", 'error')
 				return render_template('home.html') 
+
 			response_data = r.json()
 			user_info_full = response_data['result'][0]
 			user_info = {
@@ -102,14 +159,14 @@ def home_view():
 				'first_name': user_info_full['firstName'] if 'firstName' in user_info_full else "",
 				'last_name': user_info_full['lastName'] if 'lastName' in user_info_full else "",
 				'rating': user_info_full['rating'] if 'rating' in user_info_full else 0,
-				'title': get_title(int(user_info_full['rating']))[0] if 'rating' in user_info_full else "Newbie",
-				'color': get_title(int(user_info_full['rating']))[1] if 'rating' in user_info_full else "grey-text",
+				'title': get_title(int(user_info_full['rating']))[0] if 'rating' in user_info_full else "Unrated",
+				'color': get_title(int(user_info_full['rating']))[1] if 'rating' in user_info_full else "text-dark",
 				'max_rating': user_info_full['maxRating'] if 'maxRating' in user_info_full else 0,
-				'max_title': get_title(int(user_info_full['maxRating']))[0] if 'maxRating' in user_info_full else "Newbie",
-				'max_color': get_title(int(user_info_full['maxRating']))[1] if 'maxRating' in user_info_full else "grey-text",
+				'max_title': get_title(int(user_info_full['maxRating']))[0] if 'maxRating' in user_info_full else "Unrated",
+				'max_color': get_title(int(user_info_full['maxRating']))[1] if 'maxRating' in user_info_full else "text-dark",
 				'organization': user_info_full['organization'] if 'organization' in user_info_full else "",
 			}
-			flash("Connected to Codeforces server.", 'success')
+			flash("Connected to Codeforces server. Scroll down to see results.", 'success')
 			return render_template('home.html', status=status, user_info=user_info, total_unsolved=total_unsolved, final_dict=final_dict)
 		else:
 			comment = response_data['comment']
